bd4b64d22bd01d60c030f78c9adae2133d5d4feb
debiviy <daveevinoya@gmail.com>
2025-01-18 19:57:19 +0800

    Technical1

---
+++ F1-TECHINCAL/tech1.py
@@ -0,0 +1,17 @@
+print("Enter your Information")
+
+Fname = input("First Name: ")
+Lname = input("Last Name: ")
+Mname = input("Middle Name: ")
+Bday = input("Birth Date: ")
+Add = input("Home Address: ")
+Course = input("Course: ")
+Yl = input("Year level: ")
+
+print("User's Information:")
+print("Name:", Fname, Mname, Lname )
+print("Birth Date: ", Bday)
+print("Home Address: ", Add)
+print("Course: ", Course)
+print("Year Level: ", Yl)
+
